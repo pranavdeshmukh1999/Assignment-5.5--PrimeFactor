7185165035385fe911aeab55ccbd44412871d4aa
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-12 12:00:50 +0530

    Added :COMPUTE PRIME FACTORIZATION PROGRAM

--- src/com/blz/AssignFactor/PrimeFactor.java
+++ src/com/blz/AssignFactor/PrimeFactor.java
@@ -6,18 +6,18 @@ public class PrimeFactor {
 
 	public static void main(String[] args) {
 
+		System.out.println("Compute Prime Factorization Program");
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number : ");
 		int number = sc.nextInt();
 
-		for (int i = 3; i < number; i++) {
+		for (int i = 3; i <= number; i++) {
 			while (number % i == 0) {
 				System.out.println(i);
 				number = number / i;
 			}
 		}
 
-		}
 	}
-
-
+}
