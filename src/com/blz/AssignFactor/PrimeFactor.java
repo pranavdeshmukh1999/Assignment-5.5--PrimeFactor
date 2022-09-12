d6ec23c32c6374bb374d2d5d74e42beb0e0a7576
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-12 11:57:32 +0530

    Added :COMPUTE PRIME FACTORIZATION PROGRAM

---
+++ src/com/blz/AssignFactor/PrimeFactor.java
@@ -0,0 +1,23 @@
+package com.blz.AssignFactor;
+
+import java.util.Scanner;
+
+public class PrimeFactor {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int number = sc.nextInt();
+
+		for (int i = 3; i < number; i++) {
+			while (number % i == 0) {
+				System.out.println(i);
+				number = number / i;
+			}
+		}
+
+		}
+	}
+
+
